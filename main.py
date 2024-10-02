500e218e1463138a7ef17f75d0d42975f4fdfb95
Magno Efren <58579707+MagnoEfren@users.noreply.github.com>
2024-10-01 21:24:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ class Portfolio(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
         self.page = page
-        self.page.padding=0
+        self.page.padding = 0
+
+        self.primary_color = ft.colors.GREEN_500
         self.build()
 
 
     def build(self):
-        self.swicth_mode = ft.IconButton(icon=ft.icons.DARK_MODE, bgcolor="green", on_click=self.change_mode)
+        self.swicth_mode = ft.IconButton(icon=ft.icons.DARK_MODE, bgcolor=self.primary_color, on_click=self.change_mode)
         self.animation_style = ft.animation.Animation(1000, ft.AnimationCurve.EASE_OUT_CUBIC) 
 
         # Configurando el contenedor
@@ -33,42 +35,43 @@ class Portfolio(ft.Container):
                                                 horizontal_alignment= ft.CrossAxisAlignment.CENTER,
                                                 controls=[
                                                     ft.Text("Hello I'm",size=30, weight=ft.FontWeight.W_900),
-                                                    ft.Text("Magno Efren",size=30, weight=ft.FontWeight.W_900),
+                                                    ft.Text("Nombre Apellido",size=30, weight=ft.FontWeight.W_900, color=self.primary_color),
                                                     ft.Text("Estoy interesado en la creación de aplicaciones de escritorio y movil, la  \n codificación aplicaciones IoT y la enseñanza / tutoría.",size=12, text_align=ft.TextAlign.CENTER),
                                                     ft.Row(expand=True,
                                                            spacing=0,
                                                            alignment= ft.MainAxisAlignment.CENTER,
                                                            controls=[
                                                                ft.TextButton("Descargar CV", 
+                                                                    height=40,
                                                                     style=ft.ButtonStyle(
+                                                                        elevation = 20,
                                                                         shape=ft.RoundedRectangleBorder(radius=20),
-                                                                        side=ft.BorderSide(1, ft.colors.GREEN),  # Borde verde delgado
-                                                                        #padding=ft.EdgeInsets.all(10),  # Ajustar el padding
-                                                                ),
+                                                                        side=ft.BorderSide(1, self.primary_color),  
+                                                                         ),
                                                                 ),
                                                                 
-                                                               ft.ElevatedButton(content=ft.Image(src="../assets/github.png", fit=ft.ImageFit.COVER, width=30),
+                                                               ft.ElevatedButton(content=ft.Image(src="../assets/github.png", fit=ft.ImageFit.COVER, width=20),
                                                                                 style=ft.ButtonStyle(
                                                                                 shape=ft.CircleBorder(),  
-                                                                                
-                                        
+                                                                                side = ft.BorderSide(1, self.primary_color)
                                                                                 ),
-                                                                                height=50
+                                                                                height=40
                                                                                  ),
 
-                                                               ft.ElevatedButton(content=ft.Image(src="../assets/linkedin.png", fit=ft.ImageFit.COVER, width=30),
+                                                               ft.ElevatedButton(content=ft.Image(src="../assets/linkedin.png", fit=ft.ImageFit.COVER, width=20),
                                                                                 style=ft.ButtonStyle(
                                                                                 shape=ft.CircleBorder(),  # Forma circular
-                                        
+                                                                                side = ft.BorderSide(1, self.primary_color)
+                                                                                
                                                                                 ),
-                                                                                height=50
+                                                                                height=40
                                                                                  ),
-                                                                ft.ElevatedButton(content=ft.Image(src="../assets/youtube.png", fit=ft.ImageFit.COVER, width=30),
+                                                                ft.ElevatedButton(content=ft.Image(src="../assets/youtube.png", fit=ft.ImageFit.COVER, width=20),
                                                                                 style=ft.ButtonStyle(
                                                                                 shape=ft.CircleBorder(),  # Forma circular
-                                        
+                                                                                side = ft.BorderSide(1, self.primary_color)
                                                                                 ),
-                                                                                height=50
+                                                                                height=40
                                                                                  ),
                                                            ]
                                                            )
@@ -78,22 +81,30 @@ class Portfolio(ft.Container):
                                             ),
 
                                 ft.Container(expand=True,
+                                        shape=ft.BoxShape.CIRCLE,
+                                        clip_behavior= ft.ClipBehavior.HARD_EDGE,
                                         margin=20,
                                         content=ft.Image(src="../assets/foto.jpg",
-                                                            fit=ft.ImageFit.CONTAIN,
-                                                            repeat=ft.ImageRepeat.NO_REPEAT,
-                                                            border_radius=ft.border_radius.all(200),
                                                          ),   
 
                                              )
                         ]
                     ),
 
-                    ft.Container( bgcolor="green", 
+                    ft.Container(
                                  height=100,
-                                # expand=True,
-                                 #content=ft.Text("© Copyright 2024 Magno Efren - Todos los derechos Reservados",size=8, weight=ft.FontWeight.W_100, expand=True),
-                                 #
+                                 padding=10,
+                                gradient= ft.LinearGradient(colors= [self.primary_color, ft.colors.TRANSPARENT], begin=ft.Alignment(y=5, x=0)),
+                                # alignment=ft.MainAxisAlignment.CENTER,
+                              #  vertical_alignment= ft.CrossAxisAlignment.CENTER,
+                                content= ft.Row(
+                                        expand=True,
+                                        alignment=ft.MainAxisAlignment.CENTER,
+                                        #vertical_alignment= ft.CrossAxisAlignment.CENTER,
+                                        controls=[
+                                            ft.Text("© Copyright 2024 Nombre Apellido - Todos los derechos Reservados",size=12,expand=True),
+                                        ]
+                                    )
                                  )
                 ]
             )
@@ -105,8 +116,28 @@ class Portfolio(ft.Container):
             animate_offset=self.animation_style,
             offset=ft.transform.Offset(-2,0),
             expand=True,
-            bgcolor="blue"
+            margin=50,
+           content= ft.Column(
+               expand=True,
+               scroll="auto",
+               controls=[
+                   ft.ResponsiveRow(
+               expand=True,
+               controls=[
+                   ft.Container(expand=True, bgcolor="blue", col = {"md":12, "lg":6}, height=200,
+                                border_radius=20,),
+                   ft.Container( expand=True, bgcolor="blue", col = {"md":12, "lg":6}, height=200,
+                                border_radius=20),
+                   ft.Container( expand=True, bgcolor="blue", col = {"md":12, "lg":6}, height=200,
+                                border_radius=20),
+                   ft.Container( expand=True, bgcolor="blue", col = {"md":12, "lg":6}, height=200,
+                                border_radius=20),
+               ]
+           )
+               ]
+           )
         )
+
         self.frame_trabajo = ft.Container(
             animate_offset=self.animation_style,
             offset=ft.transform.Offset(-2,0),  
@@ -147,23 +178,23 @@ class Portfolio(ft.Container):
                                     ft.Text(
                                         size=20,
                                         spans=[
-                                        ft.TextSpan("Magno", style=ft.TextStyle(color=ft.colors.GREEN_200, weight=ft.FontWeight.W_900)),
-                                        ft.TextSpan("Efren", style=ft.TextStyle(color=ft.colors.GREEN_400, weight=ft.FontWeight.W_900)),
-                                        ft.TextSpan(".", style=ft.TextStyle(color=ft.colors.GREEN_600, weight=ft.FontWeight.W_900)),
+                                        ft.TextSpan("Nom", style=ft.TextStyle(color=ft.colors.GREEN_100, weight=ft.FontWeight.W_900)),
+                                        ft.TextSpan("bre", style=ft.TextStyle(color=ft.colors.GREEN_400, weight=ft.FontWeight.W_900)),
+                                        ft.TextSpan(".",style=ft.TextStyle(color=ft.colors.GREEN_800, weight=ft.FontWeight.W_900)),
                                             ]
                                         )
                                         ),
                                         ft.ResponsiveRow(
                                             alignment=ft.MainAxisAlignment.CENTER, 
-                                            spacing=10,
-                                          #  run_spacing={"xs": 10},
+                                            spacing=0,
+                                            run_spacing={"xs": 0},
                                             expand=True,
                                             controls=[
-                                        ft.TextButton("Inicio", style=ft.ButtonStyle(color=ft.colors.GREEN_400),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(0)),
-                                        ft.TextButton("Resumen", style=ft.ButtonStyle(color=ft.colors.GREEN_400),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(1)),
-                                        ft.TextButton("Trabajo", style=ft.ButtonStyle(color=ft.colors.GREEN_400),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(2)),
-                                        ft.TextButton("Contacto", style=ft.ButtonStyle(color=ft.colors.GREEN_400),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(3)),
-                                        ft.TextButton("Sobre mi", style=ft.ButtonStyle(color=ft.colors.GREEN_400),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(4)),
+                                        ft.TextButton("Inicio", style=ft.ButtonStyle(color=self.primary_color),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(0)),
+                                        ft.TextButton("Resumen", style=ft.ButtonStyle(color=self.primary_color),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(1)),
+                                        ft.TextButton("Trabajo", style=ft.ButtonStyle(color=self.primary_color),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(2)),
+                                        ft.TextButton("Contacto", style=ft.ButtonStyle(color=self.primary_color),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(3)),
+                                        ft.TextButton("Sobre mi", style=ft.ButtonStyle(color=self.primary_color),col={"xs": 12,"sm":6, "md":2},  on_click= lambda e:self.on_change_page(4)),
                                             ]
                                         ),
                                         ft.Container(
