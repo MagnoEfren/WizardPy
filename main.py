9b69483f6049a0b8f19ff412448c27fe5ad915f8
Magno Efren <58579707+MagnoEfren@users.noreply.github.com>
2024-11-12 21:21:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,165 +1,142 @@
 
+import flet as ft 
 
-import flet as ft
-class PortafolioWeb(ft.Container):
-    def __init__(self, page: ft.Page):
-        super().__init__()
-        self.page = page
-        self.page.padding = 0
-        self.page.fonts = { "Starjhol": "../assets/Starjhol.ttf"}    
-        self.text_font = "Tahoma"
-        self.page.title = "Mi Portafolio"
-        self.color_primaria = ft.colors.GREEN_500
+class CustomButton(ft.Container): 
+    def __init__(self,border_radius, bgcolor, text, data, on_click):
+        super().__init__(expand=True) 
+        
+        self.border_radius = border_radius
+        self.width = 2*self.border_radius
+        self.height = 2*self.border_radius
+        self.bgcolor = bgcolor
+        self.data = data
+        self.shadow = [
+            ft.BoxShadow(
+                offset = ft.Offset(15, 15),
+                blur_radius = 15,
+                color = "#1c9e78",
+                blur_style = ft.ShadowBlurStyle.NORMAL,
+            ),
+            ft.BoxShadow(
+                offset = ft.Offset(-15, -15),
+                blur_radius = 15,
+                color = "#25d7a3",
+                blur_style = ft.ShadowBlurStyle.NORMAL,
+            ),
+        ]
+        self.content = ft.Text(value=text, color="black", size=20, weight="bold")
+        self.alignment = ft.alignment.center
+        self.on_click = on_click
+        
+def main(page:ft.Page):
+    page.bgcolor = "#21bb8e"
 
-        self.build()
+    def get_data(e):
+        data = e.control.data
+        if data == "=":
+            try:
+                text.value = str(eval(text.value))
+            except Exception as e:
+                text.value = "ERROR"
+        elif data == "C":
+            text.value = ""
+        elif data == "AC":
+            text.value = text.value[:-1] if text.value else text.value
+        elif data == "%":
+            try:
+                text.value = str(float(text.value) / 100)
+            except ValueError:
+                text.value = "ERROR"
+        elif data == ".":
+            if not text.value or text.value[-1] not in [".", "+", "-", "/", "*"]:
+                text.value += data
+        elif data == "±":
+            text.value = "-" + text.value if text.value and text.value[0] != "-" else text.value[1:]
+        elif data in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "/", "*"]:
+            text.value += data
+        page.update()
+
+    text = ft.TextField(read_only=True, border_color="#21bb8e",text_align="right",
+                        text_style=ft.TextStyle(size=30, color ="black"))
     
-    def build(self):
+    butons = ft.Column(
+        expand = True,
+        horizontal_alignment = ft.CrossAxisAlignment.CENTER,
+        controls=[
+        ft.Row([
+            
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "1", data="1"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "2", data="2"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "3", data="3"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "+", data="+"),
 
-        self.cambiar_modo = ft.IconButton(icon= ft.icons.DARK_MODE, bgcolor= self.color_primaria, on_click= self.cambiar_modo_oscuro)
-
-        self.animation_style = ft.animation.Animation(1000, ft.AnimationCurve.EASE_OUT_CUBIC)
-
-        self.frame_inicio = ft.Container(expand= True,
-                                         animate_offset= self.animation_style, 
-                                         offset=ft.transform.Offset(0,0),
-                                        )
-
-        self.frame_servicio =  ft.Container(expand= True, bgcolor="blue",
-                                         animate_offset= self.animation_style, 
-                                         offset=ft.transform.Offset(-2,0),
-                                        )
-
-        self.titulo_resumen = ft.Text("Mi Experiencia", size=30, weight =ft.FontWeight.W_900, color= self.color_primaria)
-
-        self.frame_experiencia = ft.Container(expand=True,
-                                              
-                                              )
-
-        self.frame_eduacion = ft.Container(expand=True, visible=False,
-                                               ) 
-
-        self.frame_habilidades = ft.Container(expand=True, visible=False,
-                                               )
-
-        self.frame_resumen =  ft.Container(expand= True,  bgcolor= "green",
-                                         animate_offset= self.animation_style, 
-                                         offset=ft.transform.Offset(-2,0),
-                                        )
-
-        self.frame_contacto =  ft.Container(expand= True,  bgcolor= "red",
-                                         animate_offset= self.animation_style, 
-                                         offset=ft.transform.Offset(-2,0),
-                                         )
-
-        self.content = ft.Column(
+            ],
+            alignment = ft.MainAxisAlignment.SPACE_EVENLY,
             expand=True,
-            spacing=2,
+                          
+        ),
+        ft.Row([
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "4", data="4"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "5", data="5"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "6", data="6"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "-", data="-"),
+            ],
+            alignment = ft.MainAxisAlignment.SPACE_EVENLY,
+            expand=True,
+                            
+        ),
+
+        ft.Row([
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "7", data="7"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "8", data="8"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "9", data="9"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "x", data="*"),
+            ],
+            alignment = ft.MainAxisAlignment.SPACE_EVENLY,
+            expand=True,
+                            
+        ),
+
+        ft.Row([
+
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= ".", data="."),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "0", data="0"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "%", data="%"),
+            CustomButton( border_radius=30, bgcolor="#21bb8e", on_click= get_data, text= "÷", data="/"),
+            ],
+            alignment = ft.MainAxisAlignment.SPACE_EVENLY,
+            expand=True,
+                            
+        ),
+
+        ft.Row([
+
+            CustomButton( border_radius=25, bgcolor="#21bb8e", on_click= get_data, text= "C", data="C"),
+            CustomButton( border_radius=25, bgcolor="#21bb8e", on_click= get_data, text= "⌫", data="AC"),
+            CustomButton( border_radius=25, bgcolor="#21bb8e", on_click= get_data, text= "±", data="±"),
+            CustomButton( border_radius=25, bgcolor="#21bb8e", on_click= get_data, text= "=", data="="),
+
+            ],
+            alignment = ft.MainAxisAlignment.SPACE_EVENLY,
+            expand=True,
+                            
+        ), 
+            
+        ],
+    )
+
+    content = ft.Container(
+        expand=True,
+        padding= ft.padding.only(top=120, left=10, right=10, bottom=10),
+        content=ft.Column(
+            expand=True,
+            horizontal_alignment= ft.CrossAxisAlignment.CENTER,
+            alignment= ft.alignment.center,
             controls=[
-                ft.Container(  #Encabezado 
-                    padding=20,
-                    content= ft.Row(   
-                        expand=True,
-                        controls=[
-                            ft.Container(expand=True, margin=ft.margin.only(left=20),
-                                         content= ft.Text(size=20,
-                                                          spans=[
-                                                              ft.TextSpan("Nom", style= ft.TextStyle(color=ft.colors.GREEN_100, weight= ft.FontWeight.W_900), ),
+            text,
+            butons,
+        ])
+    )
+    page.add(content)
 
-                                                               ft.TextSpan("bre", style= ft.TextStyle(color=ft.colors.GREEN_400, weight= ft.FontWeight.W_900), ),
-                                                                
-                                                                ft.TextSpan(".", style= ft.TextStyle(color=ft.colors.GREEN_900, weight= ft.FontWeight.W_900), ),
-
-
-                                                          ]
-                                                          )
-                                         ),
-
-                            ft.ResponsiveRow(
-                                alignment= ft.MainAxisAlignment.CENTER,
-                                spacing=0,
-                                expand=True,
-                                controls=[
-                                    ft.TextButton("Inicio", style=ft.ButtonStyle(color=self.color_primaria), col={"xs":12, "sm":6, "md":3}, on_click=lambda e: self.cambiar_pagina(0)),
-                                    ft.TextButton("Servicio", style=ft.ButtonStyle(color=self.color_primaria), col={"xs":12, "sm":6, "md":3}, on_click=lambda e: self.cambiar_pagina(1)),
-                                    ft.TextButton("Resumen", style=ft.ButtonStyle(color=self.color_primaria), col={"xs":12, "sm":6, "md":3}, on_click=lambda e: self.cambiar_pagina(2)),
-                                    ft.TextButton("Contacto", style=ft.ButtonStyle(color=self.color_primaria), col={"xs":12, "sm":6, "md":3}, on_click=lambda e: self.cambiar_pagina(3)),
-                                ]
-                            ),
-                        
-                            ft.Container(width=50,
-                                         margin=ft.margin.only(right=20),
-                                         content= self.cambiar_modo                                         
-                                         )
-                        
-                        ]
-                    )
-                ),
-
-                ft.Container(
-                    expand=True,
-                    content=ft.Stack(
-                        controls=[
-                            self.frame_inicio,
-                            self.frame_servicio,
-                            self.frame_resumen,
-                            self.frame_contacto,
-                        ]
-                    )
-                ), # Cuerpo
-
-
-
-                ft.Container(
-                    padding=20,
-                    gradient= ft.LinearGradient(colors=[self.color_primaria, ft.colors.TRANSPARENT]),
-                    content= ft.Text("Copyright 2024 Nombre Apellido -  Todos los derechos recervados", size=14)
-                ), #parte inferior 
-            ]
-        )
-
-        self.page.add(self.content)
-    
-    def cambiar_pagina(self, e):
-        self.frame_inicio.offset.x = -2
-        self.frame_servicio.offset.x = -2 
-        self.frame_resumen.offset.x = -2
-        self.frame_contacto.offset.x = -2 
-
-        if e==0:
-            self.frame_inicio.offset.x = 0
-        if e==1:
-            self.frame_servicio.offset.x = 0
-        if e==2:
-            self.frame_resumen.offset.x = 0
-        if e==3:
-            self.frame_contacto.offset.x = 0
-
-        self.page.update()
-
-    def cambiar_modo_oscuro(self, e):
-        if e.control.icon =="dark_mode":
-            self.cambiar_modo.icon = ft.icons.LIGHT_MODE
-            self.page.theme_mode = "light"
-        else:
-            self.cambiar_modo.icon = ft.icons.DARK_MODE
-            self.page.theme_mode = "dark"   
-        self.page.update()        
-
-    def cambiar_pagina_resumen(self, e):
-        self.frame_experiencia.visible = False
-        self.frame_eduacion.visible  = False
-        self.frame_habilidades.visible  = False
-
-        if e==0:
-            self.frame_experiencia.visible = True
-            self.titulo_resumen.value = "Mi Experiencia"
-        elif e==1:
-            self.frame_eduacion.visible = True
-            self.titulo_resumen.value = "Mi Educación"
-        elif e==2:
-            self.frame_habilidades.visible = True
-            self.titulo_resumen.value = "Mis Habilidades"
-
-        self.page.update()
-
-ft.app(target=lambda page: PortafolioWeb(page), view= ft.WEB_BROWSER, assets_dir="assets")
+ft.app(target=main, view= ft.AppView.WEB_BROWSER)
